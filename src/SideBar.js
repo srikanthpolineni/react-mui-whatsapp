3a498f3a63a3fa38cd425b32866e6c0879ef2957
Srikanth Polineni <polineni.srikanth@gmail.com>
2021-05-24 00:29:33 +0530

    Added Profile Sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import { Box, Divider, } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box, Divider, IconButton, Typography } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import SideBarSearch from './SideBarSearch';
 import ConversationList from './ConversationList';
 import SideBarHeader from './SideBarHeader';
 
 export default function SideBar() {
+
+    const [isProfileOpen, setProfileOpen] = useState(false);
+
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%', backgroundColor: '#ededed', width: "400px" }}>
-            <SideBarHeader />
-            <Divider />
-            <SideBarSearch />
-            <Divider />
-            <ConversationList />
-        </Box>
+        <>
+            <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%', backgroundColor: '#ededed', width: "400px" }}>
+                <SideBarHeader openProfile={setProfileOpen} />
+                <Divider />
+                <SideBarSearch />
+                <Divider />
+                <ConversationList />
+            </Box>
+
+            {
+                isProfileOpen &&
+                <Box boxShadow={2}
+                    zIndex="tooltip"
+                    position="absolute"
+                    sx={{ display: 'flex', flexDirection: 'column', height: 'calc(100% - 38px)', backgroundColor: '#ededed', width: "400px" }}>
+                    <Box pt={7.6} sx={{ display: 'flex', flexDirection: 'column', backgroundColor: '#00bfa5' }}>
+                        <Box sx={{ ml: 2, marginTop: "auto", display: 'flex', flexDirection: 'row' }}>
+                            <IconButton onClick={() => setProfileOpen(false)}>
+                                <ArrowBackIcon sx={{ color: '#ffffff' }} />
+                            </IconButton>
+                            <Typography component="h1" color='#ffffff' sx={{ fontSize: '19px', marginTop: '11px', marginLeft: '20px' }}>
+                                Profile
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Box>
+            }
+        </>
     )
 }
--- src/SideBarHeader.js
+++ src/SideBarHeader.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { AppBar, Avatar, Box, IconButton, Toolbar } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-export default function SideBarHeader() {
-    const avtharNumber = Math.floor(Math.random() * 7) + 1;
+export default function SideBarHeader({ openProfile }) {
+    const avtharNumber = 1;
     return (
         <AppBar elevation={0} position="static" color="default" sx={{ backgroundColor: '#ededed', width: "400px", borderRight: '5px solid gainsboro', borderRightWidth: 'thin' }} >
             <Toolbar style={{ minWidth: '100px' }}>
-                <IconButton>
+                <IconButton onClick={() => openProfile(true)}>
                     <Avatar alt="Srikanth Polineni" src={`https://material-ui.com/static/images/avatar/${avtharNumber}.jpg`} />
                 </IconButton>
                 <Box sx={{ flexGrow: 1 }} />
